f790b41c67203ee1afea4ff58138b20b95bd9bb5
Arek Oziębło <arkadiuszozieblo@gmail.com>
2017-05-16 19:11:57 +0200

    Version 0.0.1 pre-alpha

---
+++ Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Travelion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opcjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart_Wyniki = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trackBar_Imprezy = new System.Windows.Forms.TrackBar();
+            this.trackBar_Widoki = new System.Windows.Forms.TrackBar();
+            this.trackBar_Jedzenie = new System.Windows.Forms.TrackBar();
+            this.trackBar_Pobyt = new System.Windows.Forms.TrackBar();
+            this.trackBar_Cena = new System.Windows.Forms.TrackBar();
+            this.trackBar_Cywilizacja = new System.Windows.Forms.TrackBar();
+            this.label_Imprezy = new System.Windows.Forms.Label();
+            this.label_Widoki = new System.Windows.Forms.Label();
+            this.label_Jedzenie = new System.Windows.Forms.Label();
+            this.label_Pobyt = new System.Windows.Forms.Label();
+            this.label_Cena = new System.Windows.Forms.Label();
+            this.label_Cywilizacja = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_Muzea = new System.Windows.Forms.Label();
+            this.trackBar_Muzea = new System.Windows.Forms.TrackBar();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Wyniki)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Imprezy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Widoki)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Jedzenie)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Pobyt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Cena)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Cywilizacja)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Muzea)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opcjeToolStripMenuItem,
+            this.pomocToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(720, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opcjeToolStripMenuItem
+            // 
+            this.opcjeToolStripMenuItem.Name = "opcjeToolStripMenuItem";
+            this.opcjeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.opcjeToolStripMenuItem.Text = "Opcje";
+            // 
+            // pomocToolStripMenuItem
+            // 
+            this.pomocToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramieToolStripMenuItem});
+            this.pomocToolStripMenuItem.Name = "pomocToolStripMenuItem";
+            this.pomocToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.pomocToolStripMenuItem.Text = "Pomoc";
+            // 
+            // oProgramieToolStripMenuItem
+            // 
+            this.oProgramieToolStripMenuItem.Name = "oProgramieToolStripMenuItem";
+            this.oProgramieToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.oProgramieToolStripMenuItem.Text = "O programie";
+            // 
+            // chart_Wyniki
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart_Wyniki.ChartAreas.Add(chartArea1);
+            this.chart_Wyniki.Location = new System.Drawing.Point(285, 38);
+            this.chart_Wyniki.Name = "chart_Wyniki";
+            this.chart_Wyniki.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Zdobyte Punkty";
+            this.chart_Wyniki.Series.Add(series1);
+            this.chart_Wyniki.Size = new System.Drawing.Size(431, 334);
+            this.chart_Wyniki.TabIndex = 1;
+            this.chart_Wyniki.Text = "chart1";
+            // 
+            // trackBar_Imprezy
+            // 
+            this.trackBar_Imprezy.LargeChange = 1;
+            this.trackBar_Imprezy.Location = new System.Drawing.Point(161, 38);
+            this.trackBar_Imprezy.Maximum = 5;
+            this.trackBar_Imprezy.Minimum = 1;
+            this.trackBar_Imprezy.Name = "trackBar_Imprezy";
+            this.trackBar_Imprezy.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_Imprezy.TabIndex = 2;
+            this.trackBar_Imprezy.Value = 1;
+            // 
+            // trackBar_Widoki
+            // 
+            this.trackBar_Widoki.LargeChange = 1;
+            this.trackBar_Widoki.Location = new System.Drawing.Point(161, 89);
+            this.trackBar_Widoki.Maximum = 5;
+            this.trackBar_Widoki.Minimum = 1;
+            this.trackBar_Widoki.Name = "trackBar_Widoki";
+            this.trackBar_Widoki.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_Widoki.TabIndex = 3;
+            this.trackBar_Widoki.Value = 1;
+            // 
+            // trackBar_Jedzenie
+            // 
+            this.trackBar_Jedzenie.LargeChange = 1;
+            this.trackBar_Jedzenie.Location = new System.Drawing.Point(161, 140);
+            this.trackBar_Jedzenie.Maximum = 5;
+            this.trackBar_Jedzenie.Minimum = 1;
+            this.trackBar_Jedzenie.Name = "trackBar_Jedzenie";
+            this.trackBar_Jedzenie.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_Jedzenie.TabIndex = 4;
+            this.trackBar_Jedzenie.Value = 1;
+            // 
+            // trackBar_Pobyt
+            // 
+            this.trackBar_Pobyt.LargeChange = 1;
+            this.trackBar_Pobyt.Location = new System.Drawing.Point(161, 191);
+            this.trackBar_Pobyt.Maximum = 5;
+            this.trackBar_Pobyt.Minimum = 1;
+            this.trackBar_Pobyt.Name = "trackBar_Pobyt";
+            this.trackBar_Pobyt.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_Pobyt.TabIndex = 5;
+            this.trackBar_Pobyt.Value = 1;
+            // 
+            // trackBar_Cena
+            // 
+            this.trackBar_Cena.LargeChange = 1;
+            this.trackBar_Cena.Location = new System.Drawing.Point(161, 242);
+            this.trackBar_Cena.Maximum = 5;
+            this.trackBar_Cena.Minimum = 1;
+            this.trackBar_Cena.Name = "trackBar_Cena";
+            this.trackBar_Cena.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_Cena.TabIndex = 6;
+            this.trackBar_Cena.Value = 1;
+            // 
+            // trackBar_Cywilizacja
+            // 
+            this.trackBar_Cywilizacja.LargeChange = 1;
+            this.trackBar_Cywilizacja.Location = new System.Drawing.Point(161, 293);
+            this.trackBar_Cywilizacja.Maximum = 5;
+            this.trackBar_Cywilizacja.Minimum = 1;
+            this.trackBar_Cywilizacja.Name = "trackBar_Cywilizacja";
+            this.trackBar_Cywilizacja.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_Cywilizacja.TabIndex = 7;
+            this.trackBar_Cywilizacja.Value = 1;
+            // 
+            // label_Imprezy
+            // 
+            this.label_Imprezy.AutoSize = true;
+            this.label_Imprezy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_Imprezy.Location = new System.Drawing.Point(20, 38);
+            this.label_Imprezy.Name = "label_Imprezy";
+            this.label_Imprezy.Size = new System.Drawing.Size(120, 20);
+            this.label_Imprezy.TabIndex = 8;
+            this.label_Imprezy.Text = "Epickie Imprezy";
+            this.label_Imprezy.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_Widoki
+            // 
+            this.label_Widoki.AutoSize = true;
+            this.label_Widoki.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_Widoki.Location = new System.Drawing.Point(20, 89);
+            this.label_Widoki.Name = "label_Widoki";
+            this.label_Widoki.Size = new System.Drawing.Size(128, 20);
+            this.label_Widoki.TabIndex = 9;
+            this.label_Widoki.Text = "Poetyckie Widoki";
+            this.label_Widoki.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_Jedzenie
+            // 
+            this.label_Jedzenie.AutoSize = true;
+            this.label_Jedzenie.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_Jedzenie.Location = new System.Drawing.Point(20, 140);
+            this.label_Jedzenie.Name = "label_Jedzenie";
+            this.label_Jedzenie.Size = new System.Drawing.Size(128, 20);
+            this.label_Jedzenie.TabIndex = 10;
+            this.label_Jedzenie.Text = "Pyszne Jedzenie";
+            this.label_Jedzenie.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_Pobyt
+            // 
+            this.label_Pobyt.AutoSize = true;
+            this.label_Pobyt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_Pobyt.Location = new System.Drawing.Point(20, 191);
+            this.label_Pobyt.Name = "label_Pobyt";
+            this.label_Pobyt.Size = new System.Drawing.Size(134, 20);
+            this.label_Pobyt.TabIndex = 11;
+            this.label_Pobyt.Text = "Bezpieczny Pobyt";
+            this.label_Pobyt.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_Cena
+            // 
+            this.label_Cena.AutoSize = true;
+            this.label_Cena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_Cena.Location = new System.Drawing.Point(21, 242);
+            this.label_Cena.Name = "label_Cena";
+            this.label_Cena.Size = new System.Drawing.Size(107, 20);
+            this.label_Cena.TabIndex = 12;
+            this.label_Cena.Text = "Niziutka Cena";
+            this.label_Cena.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_Cywilizacja
+            // 
+            this.label_Cywilizacja.AutoSize = true;
+            this.label_Cywilizacja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_Cywilizacja.Location = new System.Drawing.Point(21, 293);
+            this.label_Cywilizacja.Name = "label_Cywilizacja";
+            this.label_Cywilizacja.Size = new System.Drawing.Size(84, 20);
+            this.label_Cywilizacja.TabIndex = 13;
+            this.label_Cywilizacja.Text = "Cywilizacja";
+            this.label_Cywilizacja.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 41);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Powiedz gdzie jechać!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(161, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 41);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Dej mie oferty!";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label_Muzea
+            // 
+            this.label_Muzea.AutoSize = true;
+            this.label_Muzea.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_Muzea.Location = new System.Drawing.Point(21, 344);
+            this.label_Muzea.Name = "label_Muzea";
+            this.label_Muzea.Size = new System.Drawing.Size(112, 20);
+            this.label_Muzea.TabIndex = 17;
+            this.label_Muzea.Text = "Muzea/Zabytki";
+            this.label_Muzea.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // trackBar_Muzea
+            // 
+            this.trackBar_Muzea.LargeChange = 1;
+            this.trackBar_Muzea.Location = new System.Drawing.Point(161, 344);
+            this.trackBar_Muzea.Maximum = 5;
+            this.trackBar_Muzea.Minimum = 1;
+            this.trackBar_Muzea.Name = "trackBar_Muzea";
+            this.trackBar_Muzea.Size = new System.Drawing.Size(104, 45);
+            this.trackBar_Muzea.TabIndex = 16;
+            this.trackBar_Muzea.Value = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 447);
+            this.Controls.Add(this.label_Muzea);
+            this.Controls.Add(this.trackBar_Muzea);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label_Cywilizacja);
+            this.Controls.Add(this.label_Cena);
+            this.Controls.Add(this.label_Pobyt);
+            this.Controls.Add(this.label_Jedzenie);
+            this.Controls.Add(this.label_Widoki);
+            this.Controls.Add(this.label_Imprezy);
+            this.Controls.Add(this.trackBar_Cywilizacja);
+            this.Controls.Add(this.trackBar_Cena);
+            this.Controls.Add(this.trackBar_Pobyt);
+            this.Controls.Add(this.trackBar_Jedzenie);
+            this.Controls.Add(this.trackBar_Widoki);
+            this.Controls.Add(this.trackBar_Imprezy);
+            this.Controls.Add(this.chart_Wyniki);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Travelion";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Wyniki)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Imprezy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Widoki)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Jedzenie)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Pobyt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Cena)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Cywilizacja)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_Muzea)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opcjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem;
+        private System.Windows.Forms.TrackBar trackBar_Imprezy;
+        private System.Windows.Forms.TrackBar trackBar_Widoki;
+        private System.Windows.Forms.TrackBar trackBar_Jedzenie;
+        private System.Windows.Forms.TrackBar trackBar_Pobyt;
+        private System.Windows.Forms.TrackBar trackBar_Cena;
+        private System.Windows.Forms.TrackBar trackBar_Cywilizacja;
+        private System.Windows.Forms.Label label_Imprezy;
+        private System.Windows.Forms.Label label_Widoki;
+        private System.Windows.Forms.Label label_Jedzenie;
+        private System.Windows.Forms.Label label_Pobyt;
+        private System.Windows.Forms.Label label_Cena;
+        private System.Windows.Forms.Label label_Cywilizacja;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label_Muzea;
+        private System.Windows.Forms.TrackBar trackBar_Muzea;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart_Wyniki;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Travelion
+{
+
+    public partial class Form1 : Form
+    {
+        public static Form1 _Form1;
+        public List<TrackBar> TrackBars = new List<TrackBar>();
+        public List<Country> Countries = new List<Country>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            _Form1 = this;
+
+            //Zmiana typu charta na poziome bary
+            chart_Wyniki.Series["Zdobyte Punkty"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.RangeBar;
+
+            chart_Wyniki.Series["Zdobyte Punkty"].IsValueShownAsLabel = true;
+
+            //Uzupełnienie listy Barów
+            TrackBars.Add(trackBar_Imprezy); TrackBars.Add(trackBar_Widoki); TrackBars.Add(trackBar_Jedzenie); TrackBars.Add(trackBar_Pobyt); TrackBars.Add(trackBar_Cena); TrackBars.Add(trackBar_Cywilizacja); TrackBars.Add(trackBar_Muzea);
+
+            //Deklaracje kolejnych krajów razem z mnożnikami
+            //W kolejności Imprezy, Widoki, Jedzenie, Pobyt, Cena, Cywilizacja, Muzea, skala 1-5
+            Country portugal = new Country("Portugalia", 4, 4, 2, 4, 2, 4, 2);
+            Country england = new Country("Anglia", 2, 2, 2, 2, 2, 4, 5);
+            Country honolulu = new Country("Honolulu", 5, 4, 3, 5, 1, 1, 1);
+            Country poland = new Country("Polska", 3, 2, 4, 5, 5, 1, 4);
+            Country india = new Country("Indie", 4, 2, 1, 3, 5, 1, 2);
+            Country france = new Country("Francja", 5, 4, 3, 1, 1, 5, 5);
+            Country czech = new Country("Czechy", 4, 2, 5, 5, 4, 4, 3);
+            Country russia = new Country("Rosja", 2, 3, 3, 2, 5, 2, 3);
+            Country niger = new Country("Niger", 1, 3, 1, 1, 1, 1, 1);
+            Country china = new Country("Chiny", 3, 5, 4, 3, 2, 4, 2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalculateTop5();
+        }
+
+        public void CalculateTop5()
+        {
+            chart_Wyniki.Series["Zdobyte Punkty"].Points.Clear();
+
+            List<int> User_Multipliers = new List<int>();
+
+            foreach (Country country in Countries)
+            {
+                country.points = 0;
+            }
+
+            foreach (TrackBar _trackbar in TrackBars)       // Zebranie wyników podanych przez naszego dzielnego usera. Sprostajmy im.
+            {
+                User_Multipliers.Add(_trackbar.Value);
+            }
+
+           foreach (Country country in Countries)
+            {
+                for (int i=0; i < 7; i++)
+                {
+                    country.points += country.multipliers[i] * User_Multipliers[i];     //Przydzielenie punktów dla każdego kraju
+                }
+            }
+
+            List<Country> SortedCountries = Countries.OrderByDescending(o => o.points).ToList(); // Zapytanie Linq, sortowanie malejąco wg punktów
+
+            for (int i=0;i<5;i++)
+            {
+                chart_Wyniki.Series["Zdobyte Punkty"].Points.Add(SortedCountries[i].points);
+                chart_Wyniki.Series["Zdobyte Punkty"].Points[i].Label = (SortedCountries[i].Name + ": " + SortedCountries[i].points.ToString() + " punktów");
+                //W tym miejscu wpierdalamy to wszystko w charta głównego
+            }
+
+        }
+    }
+
+    public class Country
+    {
+        public int points = 0;
+        public int[] multipliers = new int[7];
+        public string Name = "";
+
+        public Country (string _name, int _points_Imprezy, int _points_Widoki, int _points_Jedzenie, int _points_Pobyt, int _points_Cena, int _points_Cywilizacja, int _points_Muzea)
+        {
+            Name = _name;
+            multipliers[0] = _points_Imprezy;
+            multipliers[1] = _points_Widoki;
+            multipliers[2] = _points_Jedzenie;
+            multipliers[3] = _points_Pobyt;
+            multipliers[4] = _points_Cena;
+            multipliers[5] = _points_Cywilizacja;
+            multipliers[6] = _points_Muzea;
+            Form1._Form1.Countries.Add(this);   // Dorzucenie stworzonego kraju do tablicy wszystkich krajów
+        }
+
+    }
+
+}
